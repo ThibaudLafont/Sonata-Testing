c64f8e353623b401f10bd8e425d406cc14b7ca17
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-17 12:37:25 +0200

    Set name of custom image provider in provider selection

--- src/Application/Sonata/MediaBundle/Provider/CustomImageProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/CustomImageProvider.php
@@ -2,11 +2,21 @@
 namespace Application\Sonata\MediaBundle\Provider;
 
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\CoreBundle\Model\Metadata;
 use Sonata\MediaBundle\Model\MediaInterface;
 use Sonata\MediaBundle\Provider\ImageProvider;
 
 class CustomImageProvider extends ImageProvider
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProviderMetadata()
+    {
+        return new Metadata('Image', $this->getName().'.description', false, 'SonataMediaBundle', ['class' => 'fa fa-picture-o']);
+    }
+
     /**
      * {@inheritdoc}
      */
