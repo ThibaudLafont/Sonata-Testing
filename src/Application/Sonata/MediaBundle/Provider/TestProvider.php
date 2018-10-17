f6b3a05139806c589450b1f4c7ddd291459e5ee9
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-17 12:10:45 +0200

    Update the image provider & edit form template

--- src/Application/Sonata/MediaBundle/Provider/TestProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/TestProvider.php
@@ -1,9 +1,19 @@
 <?php
 namespace Application\Sonata\MediaBundle\Provider;
 
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\MediaBundle\Model\MediaInterface;
 use Sonata\MediaBundle\Provider\ImageProvider;
 
 class TestProvider extends ImageProvider
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildEditForm(FormMapper $formMapper)
+    {
+        $formMapper->add('name');
+        $formMapper->add('description');
+    }
 
 }
