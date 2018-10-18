495aa0bd87a895e78b5a06630d9df731c4070418
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-18 14:59:16 +0200

    Improve Vimeo getCode()

--- src/Application/Sonata/MediaBundle/Provider/CustomVimeoProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/CustomVimeoProvider.php
@@ -8,4 +8,21 @@ class CustomVimeoProvider extends VimeoProvider
 
     use BaseVideoProvider;
 
+    /**
+     * @param MediaInterface $media
+     */
+    protected function fixBinaryContent(MediaInterface $media)
+    {
+        if (!$media->getBinaryContent()) {
+            return;
+        }
+
+        if (preg_match(
+                "%(http|https)?:\/\/(www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|)(\d+)(?:|\/\?)%",
+                $media->getBinaryContent(), $matches)
+        ) {
+            $media->setBinaryContent($matches[2]);
+        }
+    }
+
 }
