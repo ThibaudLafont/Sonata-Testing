b4013a85554b0c305ad1b40e2d485737deb0b1c3
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-18 12:48:17 +0200

    Inquire listField for MediaAdmin

--- src/Application/Sonata/MediaBundle/Admin/MediaAdmin.php
+++ src/Application/Sonata/MediaBundle/Admin/MediaAdmin.php
@@ -2,10 +2,21 @@
 namespace Application\Sonata\MediaBundle\Admin;
 
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\MediaBundle\Admin\BaseMediaAdmin;
 
 class MediaAdmin extends BaseMediaAdmin
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+        ;
+    }
+
     /**
      * {@inheritdoc}
      */
