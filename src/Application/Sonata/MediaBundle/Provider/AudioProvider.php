f0d84f6833e0f681cc12f243c60a7af97fc8b242
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-18 16:12:55 +0200

    Add glyphicon to provider selector

--- src/Application/Sonata/MediaBundle/Provider/AudioProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/AudioProvider.php
@@ -1,11 +1,20 @@
 <?php
 namespace Application\Sonata\MediaBundle\Provider;
 
+use Sonata\CoreBundle\Model\Metadata;
 use Sonata\MediaBundle\Model\MediaInterface;
 use Sonata\MediaBundle\Provider\FileProvider;
 
 class AudioProvider extends FileProvider
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProviderMetadata()
+    {
+        return new Metadata($this->getName(), $this->getName().'.description', false, 'SonataMediaBundle', ['class' => 'glyphicon glyphicon-music']);
+    }
 
     /**
      * {@inheritdoc}
